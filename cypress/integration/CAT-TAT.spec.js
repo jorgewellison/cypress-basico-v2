8d6edb7f08cfa99d039408b1e36ad68f63d4abe7
Jorge <jorgewellison1@gmail.com>
2022-08-28 18:12:11 -0300

    Digitando em campos e clicando em elementos

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -1,10 +1,19 @@
 /// <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+describe('Central de Atendimento ao Cliente TAT', function () {
+    beforeEach(function () {
         cy.visit('./src/index.html')
-
-        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-        
     })
-  })
+    it('verifica o título da aplicação', function () {
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+
+    })
+
+    it.only('preenche os campos obrigatórios e envia o formulário', function() {
+        cy.get('#firstName').type('Jorge')
+        cy.get('#lastName').type('Wellison')
+        cy.get('#email').type('teste@gmail.com')
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+    })
+})
