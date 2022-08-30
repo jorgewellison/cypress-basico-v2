e4922959d46070ddedca280be49d18087742c741
Jorge Wellison <jorge.wellison@phoebustecnologia.com.br>
2022-08-29 23:19:41 -0300

    exercicio extra 8

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -15,7 +15,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#lastName').type('Wellison')
         cy.get('#email').type('teste@gmail.com')
         cy.get('#open-text-area').type(longText, { delay: 0})
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
 
         cy.get('.success').should('be.visible')
     })
@@ -25,7 +25,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#lastName').type('Wellison')
         cy.get('#email').type('teste@gmail,com')
         cy.get('#open-text-area').type('teste')
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
 
         cy.get('.error').should('be.visible')
     })
@@ -42,7 +42,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#email').type('teste@gmail.com')
         cy.get('#phone-checkbox').click()
         cy.get('#open-text-area').type('Text')
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
 
         cy.get('.error').should('be.visible')
     })
@@ -71,12 +71,14 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     })
 
     it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')
     })
 
-    it.only('envia o formuário com sucesso usando um comando customizado', function(){
+    it('envia o formuário com sucesso usando um comando customizado', function(){
         cy.fillMandatoryFieldsAndSubmit()
         cy.get('.success').should('be.visible')
     })
+
+
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,5 +3,5 @@ Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function () {
     cy.get('#lastName').type('Wellison')
     cy.get('#email').type('teste@gmail.com')
     cy.get('#open-text-area').type('teste')
-    cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
 })
